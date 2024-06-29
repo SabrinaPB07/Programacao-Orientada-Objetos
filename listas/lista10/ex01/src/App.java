d147a0aea00e2a7aa818bddf3d043b34d7c7aeb2
SabrinaPB07 <sabrinapaulabruno@gamil.com>
2024-06-28 22:47:13 -0300

    lista10

---
+++ listas/lista10/ex01/src/App.java
@@ -0,0 +1,7 @@
+import locadora.Filme;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Filme filme = new Filme("A fantastica fabrica de chocolate", "Tim Burton)
+    }
+}
